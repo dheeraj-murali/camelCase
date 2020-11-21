0ff54088d819106808d300415fd236057b97cfe9
Dheeraj <410dheeraj@gmail.com>
2020-11-21 14:54:29 +0530

    ci: adding svg import

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -24,10 +24,18 @@ module.exports = {
         background_color: `#FFFFFF`,
         theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
+        icon: `src/assets/images/favicon.ico`, // This path is relative to the root of the site.
       },
     },
     "@chakra-ui/gatsby-plugin",
     `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/, // See below to configure properly
+        },
+      },
+    },
   ],
 }
